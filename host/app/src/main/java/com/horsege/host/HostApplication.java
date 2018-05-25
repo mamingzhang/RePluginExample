8e350006eecbd1f2101910181d50ccd3de7c4c5e
mamingzhang <mamingzhang@shuzijiayuan.com>
2018-05-25 16:01:25 +0800

    启动外置插件调试

--- host/app/src/main/java/com/horsege/host/HostApplication.java
+++ host/app/src/main/java/com/horsege/host/HostApplication.java
@@ -1,6 +1,46 @@
 package com.horsege.host;
 
+import android.content.Context;
+import android.content.Intent;
+
 import com.qihoo360.replugin.RePluginApplication;
+import com.qihoo360.replugin.RePluginCallbacks;
+import com.qihoo360.replugin.RePluginConfig;
+import com.qihoo360.replugin.RePluginEventCallbacks;
 
 public class HostApplication extends RePluginApplication {
+
+    @Override
+    protected RePluginConfig createConfig() {
+        RePluginConfig config = new RePluginConfig();
+        config.setUseHostClassIfNotFound(true);
+        config.setVerifySign(!BuildConfig.DEBUG);
+        config.setEventCallbacks(new HostEventCallback(this));
+        return config;
+    }
+
+    @Override
+    protected RePluginCallbacks createCallbacks() {
+        return new HostCallbacks(this);
+    }
+
+    private class HostCallbacks extends RePluginCallbacks {
+
+        public HostCallbacks(Context context) {
+            super(context);
+        }
+
+        @Override
+        public boolean onPluginNotExistsForActivity(Context context, String plugin, Intent intent, int process) {
+            return super.onPluginNotExistsForActivity(context, plugin, intent, process);
+        }
+    }
+
+    private class HostEventCallback extends RePluginEventCallbacks{
+
+        public HostEventCallback(Context context) {
+            super(context);
+        }
+    }
+
 }
---
+++ host/app/src/main/java/com/horsege/host/activity/MainActivity.java
@@ -0,0 +1,80 @@
+package com.horsege.host.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.horsege.host.R;
+import com.qihoo360.replugin.RePlugin;
+import com.qihoo360.replugin.model.PluginInfo;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.externalPluginBtn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.externalPluginBtn: {
+                installExternalPlugin();
+                break;
+            }
+        }
+    }
+
+    private void installExternalPlugin() {
+        String demo1PluginPath = getFilesDir() + File.separator + "plugin1.apk";
+        File demo1PluginFile = new File(demo1PluginPath);
+        if (demo1PluginFile.exists()) {
+            demo1PluginFile.delete();
+        }
+
+        String demo1ApkPath = "external" + File.separator + "plugin1.apk";
+
+        InputStream inputStream = null;
+        FileOutputStream outputStream = null;
+        try {
+            inputStream = getAssets().open(demo1ApkPath);
+            outputStream = openFileOutput(demo1PluginPath, Context.MODE_PRIVATE);
+            int count = 0;
+            byte[] buffer = new byte[1024];
+            while ((count = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, count);
+            }
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null)
+                    inputStream.close();
+                if (outputStream != null)
+                    outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        PluginInfo pluginInfo = null;
+        if (demo1PluginFile.exists()) {
+            pluginInfo = RePlugin.install(demo1PluginPath);
+        }
+
+        if (pluginInfo != null) {
+            RePlugin.startActivity(this, RePlugin.createIntent(pluginInfo.getName(), "com.horsege.plugin1.activity.MainActitity"));
+        }
+    }
+}
